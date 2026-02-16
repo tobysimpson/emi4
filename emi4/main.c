0c3af9299660d4af15e5d4eb28ac1342f5be24b6
toby <tobygeneva@hotmail.com>
2026-02-16 14:59:00 +0100

    Initial Commit

---
+++ emi4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  emi4
+//
+//  Created by Toby Simpson on 16.02.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
